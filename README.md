340dc389d7f498f13b720413a2d8627be1102b17
Nassim <nassim.habbash@gmail.com>
2020-06-11 00:31:05 +0200

    fixed some bugs

--- README.md
+++ README.md
@@ -1,22 +1,24 @@
-# Title
-> Subtitle
+# Truth Finder
+> Data fusion and truth discovery from multiple conflicting information providers
 
 ## Overview
-TODO
+This work implements the TruthFinder algorithm as presented in the paper [Truth Discovery with Multiple Conflicting Information Providers on the Web](http://web.cs.ucla.edu/~yzsun/classes/2014Spring_CS7280/Papers/Trust/kdd07_xyin.pdf). TruthFinder is an algorithm that tries to solve the veracity problem in an iterative process, thus find the most probable true value for an object given a set of possible conflicting values.
 
 #### [Documentatation](docs/report.pdf)
 #### [Presentation](docs/presentation.pdf)
 
 # Prerequisites
 * Python 3.7 or higher
-* Conda or Virtualenv
+* Virtualenv
+* Microsoft Visual C++ 14.0 for `fuzzywuzzy[speedup]`, otherwise just use `fuzzywuzzy`
 
 ## Installation
 ```sh
-$ git clone https://github.com/nhabbash/temptitle
-$ cd temptitle
-$ conda env create -p .\cenv -f .\environment.yml # using conda
-$ jupyter nbextensions_configurator enable --user # optional
+$ git clone https://github.com/nhabbash/truth_discovery
+$ cd truth_discovery
+$ python -m venv .venv
+$ .venv/Scripts/Activate.ps1
+$ pip install -r requirements
 ```
 #
 ## Authors
--- truthfinder.py
+++ truthfinder.py
@@ -1,22 +1,25 @@
 import numpy as np
 import pandas as pd
-import jellyfish
-
-pd.options.mode.chained_assignment = 'raise'
+from fuzzywuzzy import fuzz
 
 class TruthFinder(object):
     '''
     TruthFinder model implementation, finds true values about objects from conflicting sources (Veracity problem).
 
     Attributes:
-        df (DataFrame): Dataframe containing the data,
-        TODO
+        df (DataFrame): DataFrame containing the data,
+        fact (string): Fact/Attribute column name in the DataFrame
+        obj (string): Object/Identifier column name in the DataFrame
+        implication (function): Similarity function between strings
+        dampening_factor (float): Dampening factor (omega) to account for source dependance
+        relatedness_factor (float): Relatedness factor (rho) to account for the influence of related facts
+        base_sim (float): Threshold for positive implication
     '''
-    def __init__(self, df, fact, obj, implication=None, dampening_factor=0.3, relatedness_factor=0.5):
+    def __init__(self, df, fact, obj, implication = None, dampening_factor = 0.3, relatedness_factor = 0.5, base_sim = 0.5):
         self.df = df
         self.fact = fact
         self.object = obj
-
+    
         if implication==None:
             self.implication = self._std_implication
         else:
@@ -24,11 +27,15 @@ class TruthFinder(object):
 
         self.dampening_factor = dampening_factor
         self.relatedness_factor = relatedness_factor
+        self.base_sim = base_sim
 
+    def compute(self, max_it = 10, tolerance = 0.001, initial_trust = 0.9):
+        '''
+        Iterative computation of fact confidences and source trustworthiness until stability
+        '''
 
-    def compute(self, max_it = 200, tolerance = 0.001, initial_trust = 0.9):
         self.df['trust'] = initial_trust
-        self.df['confidence'] = 0
+        self.df['confidence'] = 0.0
 
         for i in range(max_it):
             t1 = self.df.drop_duplicates("source")["trust"]
@@ -36,7 +43,7 @@ class TruthFinder(object):
             self.compute_fact_confidence()
             self.compute_source_trust()
 
-            t2 = t1 = self.df.drop_duplicates("source")["trust"]
+            t2 = self.df.drop_duplicates("source")["trust"]
 
             # Convergence of the process is measured by the change in trustworthiness of sources
             error = (t1 @ t2.T) / (np.linalg.norm(t1)*np.linalg.norm(t2))
@@ -45,7 +52,7 @@ class TruthFinder(object):
             if error > tolerance:
                 break
         
-        return self.df
+        return self.extract_truth()
 
     def compute_fact_confidence(self):
         '''
@@ -53,7 +60,7 @@ class TruthFinder(object):
         '''
         for obj in self.df[self.object].unique():
             idx = self.df[self.object] == obj
-            data = self.df[idx].copy()
+            data = self.df.loc[idx].copy()
             data = self._initial_confidence(data)
             data = self._adjust_confidence_related(data)
             data = self._adjust_confidence_dependance(data)
@@ -63,12 +70,11 @@ class TruthFinder(object):
         '''
         Computes a fact's initial confidence score by summing all its sources' trustworthiness scores
         '''
-
         for i, row in data.iterrows():
             # Extracting source trustworthiness
             t_s = data.loc[data[self.fact] == row[self.fact], "trust"]
             # Computing source trustworthiness score (tau_s) and fact confidence score (sigma_f) at once
-            sigma_f = sum(np.log(1-t) for t in t_s)
+            sigma_f = sum(-np.log(1-t) for t in t_s)
             data.at[i, "confidence"] = sigma_f
         return data
 
@@ -76,24 +82,29 @@ class TruthFinder(object):
         '''
         Adjust a fact's confidence score by how influenced it is by other similar facts
         '''
-
         adjusted_confidences = {}
         for i, i_row in data.iterrows():
             f1 = i_row[self.fact]
-            sigma_s = 0
-            for j, j_row in self.df.drop_duplicates(self.fact).iterrows():
+            sigma_s = 0.0
+            for j, j_row in data.drop_duplicates(self.fact).iterrows():
                 f2 = j_row[self.fact]
                 if f1 == f2:
                     continue
                 sigma_s += j_row["confidence"] * self.implication(f2, f1)
+                
+            if(f1 in ["Einstein", "Albert Einstein"] or f2 in ["Einstein", "Albert Einstein"]):
+                print(f1)
+                print(f2)
+                print(sigma_s)
+
             adjusted_confidences[i] = self.relatedness_factor * sigma_s + i_row["confidence"]
+            
         
         data["confidence"] = adjusted_confidences.values()
-        
         return data
 
     def _std_implication(self, f1, f2):
-        return jellyfish.jaro_winkler(f1.lower(), f2.lower())
+        return (fuzz.token_set_ratio(f1.lower(), f2.lower()) / 100) - self.base_sim
 
     def _adjust_confidence_dependance(self, data):
         '''
@@ -113,8 +124,20 @@ class TruthFinder(object):
            idx = self.df["source"] == source
            t_s = self.df.loc[idx, "confidence"]
            self.df.loc[idx, "trust"] = sum(t_s) / len(t_s)
-            
-        
+    
+    def extract_truth(self):
+        '''
+        Extracts the singular objects with their maximum confidence facts
+        '''
+        object_collection = pd.DataFrame(columns=self.df.columns)
+        for obj in self.df[self.object].unique():
+            idx = self.df[self.object] == obj
+            data = self.df.loc[idx].copy()
+            true_fact = data.loc[data["confidence"].idxmax()]
+            object_collection = object_collection.append([true_fact], ignore_index=True)
+        return object_collection
+
+
 #### Other
     def _adjust_confidence_related2(self, data):
         '''
